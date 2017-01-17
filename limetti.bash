7b87cbd96d56227b9cc583d65bbe27333f08d8fd
Antti Tapio <antti.tapio@iki.fi>
2017-01-17 22:11:46 +0200

    allow overriding pup and jq paths

--- limetti.bash
+++ limetti.bash
@@ -6,8 +6,8 @@ CHANNEL=${CHANNEL:-lounas}
 BOT_USERNAME=${BOT_USERNAME:-limetti}
 BOT_EMOJI=${BOT_EMOJI:-stew}
 
-PUP_EXECUTABLE=pup
-JQ_EXECUTABLE=jq
+PUP=${PUP:-pup}
+JQ=${JQ:-jq}
 
 echoerr() { echo "$@" 1>&2; }
 
@@ -19,8 +19,8 @@ then
 fi
 
 # Confirm that tools exist
-command -v "$PUP_EXECUTABLE" >/dev/null 2>&1 || (echoerr "pup not found"; exit 2)
-command -v "$JQ_EXECUTABLE" >/dev/null 2>&1 || (echoerr "jq not found"; exit 3)
+command -v "$PUP" >/dev/null 2>&1 || (echoerr "pup not found"; exit 2)
+command -v "$JQ" >/dev/null 2>&1 || (echoerr "jq not found"; exit 3)
 
 SLACK_JSON="{ text: ., channel: \"#$CHANNEL\", \"username\": \"$BOT_USERNAME\", \"icon_emoji\": \":$BOT_EMOJI:\"}"
 
@@ -48,7 +48,7 @@ esac
 
 if [ "x$div_id" != "x" ]
 then
-    todays_menu=$(curl -s "$MENU_URL" | $PUP_EXECUTABLE "div#$div_id div.data h4 text{}")
+    todays_menu=$(curl -s "$MENU_URL" | $PUP "div#$div_id div.data h4 text{}")
     #echo "$todays_menu"
-    echo "$todays_menu" | $JQ_EXECUTABLE -R . | jq -s "join(\"\n\") | $SLACK_JSON" | curl -s -X POST --data-urlencode "payload@-" "$WEBHOOK_URL"
+    echo "$todays_menu" | $JQ -R . | jq -s "join(\"\n\") | $SLACK_JSON" | curl -s -X POST --data-urlencode "payload@-" "$WEBHOOK_URL"
 fi
--- mau-kas.bash
+++ mau-kas.bash
@@ -6,8 +6,8 @@ CHANNEL=${CHANNEL:-lounas}
 BOT_USERNAME=${BOT_USERNAME:-MAU-KAS}
 BOT_EMOJI=${BOT_EMOJI:-stew}
 
-PUP_EXECUTABLE=pup
-JQ_EXECUTABLE=jq
+PUP=${PUP:-pup}
+JQ=${JQ:-jq}
 
 echoerr() { echo "$@" 1>&2; }
 
@@ -19,10 +19,10 @@ then
 fi
 
 # Confirm that tools exist
-command -v "$PUP_EXECUTABLE" >/dev/null 2>&1 || (echoerr "pup not found"; exit 2)
-command -v "$JQ_EXECUTABLE" >/dev/null 2>&1 || (echoerr "jq not found"; exit 3)
+command -v "$PUP" >/dev/null 2>&1 || (echoerr "pup not found"; exit 2)
+command -v "$JQ" >/dev/null 2>&1 || (echoerr "jq not found"; exit 3)
 
 SLACK_JSON="{ text: ., channel: \"#$CHANNEL\", \"username\": \"$BOT_USERNAME\", \"icon_emoji\": \":$BOT_EMOJI:\"}"
 
-todays_menu=$(curl -s "$MENU_URL" | $PUP_EXECUTABLE -l 1 'div.restaurant_menu div.restaurant_menuitemname text{}'|grep -v "^$")
-echo "$todays_menu" | $JQ_EXECUTABLE -R . | jq -s "join(\"\n\") | $SLACK_JSON" | curl -s -X POST --data-urlencode "payload@-" "$WEBHOOK_URL"
+todays_menu=$(curl -s "$MENU_URL" | $PUP -l 1 'div.restaurant_menu div.restaurant_menuitemname text{}'|grep -v "^$")
+echo "$todays_menu" | $JQ -R . | jq -s "join(\"\n\") | $SLACK_JSON" | curl -s -X POST --data-urlencode "payload@-" "$WEBHOOK_URL"
